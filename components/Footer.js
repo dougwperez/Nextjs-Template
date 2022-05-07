158bd6918281c21461abaa52f70eec349ba51fad
Doug Perez <perezcpt@gmail.com>
2022-05-06 18:00:43 -0700

    part 3 done

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Copyright 2021 Panop Software</div>;
+};
+
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>
         Lorem Ipsum passages, and more recently with desktop publishing software
         like Aldus PageMaker including versions of Lorem Ipsum.
       </p>
+      <Footer />
     </div>
   );
 }

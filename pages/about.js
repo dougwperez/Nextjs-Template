ad2581d4400fa2722cc0c7bf5bbbd8225ab5bb26
Doug Perez <perezcpt@gmail.com>
2022-05-09 21:20:16 -0700

    part 9 done

--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,24 @@
+import Head from "next/head";
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem Ipsum passages, and more recently with desktop publishing software
-        like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-      <p>
-        Lorem Ipsum passages, and more recently with desktop publishing software
-        like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem Ipsum passages, and more recently with desktop publishing
+          software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <p>
+          Lorem Ipsum passages, and more recently with desktop publishing
+          software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+      </div>
+    </>
   );
 };
 
--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,21 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.title}>
-      <h1>Homepage</h1>
-      <p className={styles.text}>
-        Lorem Ipsum passages, and more recently with desktop publishing software
-        like Aldus PageMaker including versions of Lorem Ipsum.
-      </p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>
+          Lorem Ipsum passages, and more recently with desktop publishing
+          software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }

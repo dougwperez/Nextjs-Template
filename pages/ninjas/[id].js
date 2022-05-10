318cc941ed58b93f72546fc9ec4e495c9a2d400e
Doug Perez <perezcpt@gmail.com>
2022-05-10 09:21:05 -0700

    part 12 done

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,18 @@
+export const getStaticPaths = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = res.json();
+
+  const paths = data.map((ninja) => {
+    return {
+      params: { id: ninja.id.toString() },
+    };
+  });
+
+  return {
+    paths: [{}, {}, { params: { id } }],
+  };
+};
+
 const Details = () => {
   return (
     <div>
